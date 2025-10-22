4c357bbc12b9b83319996a3762b70d71038fb443
EnollaTomazic <59646474+EnollaTomazic@users.noreply.github.com>
2025-10-22 12:59:47 +0000

    chore: scope geist font preloads

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,6 +1,59 @@
-import { GeistMono } from "geist/font/mono";
-import { GeistSans } from "geist/font/sans";
+import localFont from 'next/font/local'
 
-export const geistSansClassName = GeistSans.className;
-export const geistSansVariable = GeistSans.variable;
-export const geistMonoVariable = GeistMono.variable;
+const geistSans = localFont({
+  variable: '--font-geist-sans',
+  display: 'swap',
+  preload: true,
+  src: [
+    {
+      path: '../../node_modules/geist/dist/fonts/geist-sans/Geist-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../node_modules/geist/dist/fonts/geist-sans/Geist-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../node_modules/geist/dist/fonts/geist-sans/Geist-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+  ],
+})
+
+const geistMonoCore = localFont({
+  variable: '--font-geist-mono',
+  display: 'swap',
+  preload: true,
+  src: [
+    {
+      path: '../../node_modules/geist/dist/fonts/geist-mono/GeistMono-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+  ],
+})
+
+const geistMonoExtras = localFont({
+  variable: '--font-geist-mono',
+  display: 'swap',
+  preload: false,
+  src: [
+    {
+      path: '../../node_modules/geist/dist/fonts/geist-mono/GeistMono-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../node_modules/geist/dist/fonts/geist-mono/GeistMono-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+  ],
+})
+
+export const geistSansClassName = geistSans.className
+export const geistSansVariable = geistSans.variable
+export const geistMonoVariable = `${geistMonoCore.variable} ${geistMonoExtras.variable}`
