e81ded886c65a73d27a953e8505987616003c147
EnollaTomazic <59646474+EnollaTomazic@users.noreply.github.com>
2025-11-14 13:38:02 +0100

    refactor: unify planner overview cards

--- src/components/home/home-landing/PlannerOverview.tsx
+++ src/components/home/home-landing/PlannerOverview.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import * as React from "react";
+import { Card } from "../Card";
 import { CalendarCard, FocusCard, MomentumCard, SummaryCard } from "./cards";
 import type { PlannerOverviewProps } from "./types";
 import { cn } from "@/lib/utils";
@@ -25,10 +26,18 @@ export function PlannerOverview({
           className,
         )}
       >
-        <SummaryCard {...summary} />
-        <FocusCard {...focus} />
-        <MomentumCard {...goals} />
-        <CalendarCard {...calendar} />
+        <Card as="section" className="col-span-12 h-full md:col-span-6 lg:col-span-4">
+          <SummaryCard {...summary} />
+        </Card>
+        <Card as="section" className="col-span-12 h-full md:col-span-6 lg:col-span-4">
+          <FocusCard {...focus} />
+        </Card>
+        <Card as="section" className="col-span-12 h-full md:col-span-6 lg:col-span-4">
+          <MomentumCard {...goals} />
+        </Card>
+        <Card as="section" className="col-span-12 h-full md:col-span-6 lg:col-span-4">
+          <CalendarCard {...calendar} />
+        </Card>
       </div>
     ),
     [calendar, className, focus, goals, summary],
